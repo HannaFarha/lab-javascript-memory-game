c7a010e16ce9bf7d42f5ea1ec97a448200cdd0c9
HannaFarha <johnnyfarha1@gmail.com>
2023-11-29 19:07:10 +0100

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
+class MemoryGame {
+  constructor(cards) {
+    this.cards = cards;
+    // add the rest of the class properties here
+    this.pickedCards=[];
+    this.pairsClicked=0;
+    this.pairsGuessed=0;
+  }
+
+  shuffleCards() {
+    // ... write your code here
+  }
+
+  checkIfPair(card1, card2) {
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    }
+    return false;
+ }
+
+ checkIfFinished() {
+    if (this.pairsGuessed === this.getTotalPairs()) {
+      // Game finished logic
+    }
+ }
+
+ getTotalPairs() {
+    // Logic to get total pairs in the game
+ }
+}
+
+
+
+
+
+
 const cards = [
   { name: 'aquaman', img: 'aquaman.jpg' },
   { name: 'batman', img: 'batman.jpg' },
@@ -35,7 +73,7 @@ window.addEventListener('load', (event) => {
         <div class="back" name="${pic.img}"></div>
         <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
       </div>
-    `;
+      `;
   });
 
   // Add all the divs to the HTML
@@ -44,7 +82,10 @@ window.addEventListener('load', (event) => {
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
+      html += `<div class="card turned" data-card-name="${card.name}">
+      <div class="back" name="${card.img}"></div>
+      <div class="front" style="background: url(img/${card.img}) no-repeat"></div>
+    </div>`
       console.log(`Card clicked: ${card}`);
     });
   });
--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,43 @@
+
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards=[];
+    this.pairsClicked=0;
+    this.pairsGuessed=0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random());
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+   }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
-  }
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    }
+    return false;
+ }
 
-  checkIfFinished() {
-    // ... write your code here
-  }
+ checkIfFinished() {
+  if (this.pairsGuessed === this.cards.length / 2) {
+    return true;}
+    else if(this.pairsGuessed !== this.cards.length / 2){
+      return false;
+    }
+ 
+      // Game finished logic
+    }
+ 
+
+ getTotalPairs() {
+    // Logic to get total pairs in the game
+ }
+
 }
